55378af5aeef5d762f93b16ccdc52f3274d4e987
Aaron <sapblatt11@gmail.com>
2024-07-01 11:06:27 -0400

    Split into 2 classes, fragmenter and fragmentanalyzer

--- src/Fragmenter.py
+++ src/Fragmenter.py
@@ -4,78 +4,19 @@ import tqdm
 import tomllib
 
 
-class UnalignedSeqFinder:
+class FragAnalyzer:
     def __init__(self, output_file="results"):
-        self.sequence_dict = {}
-        self.unaligned_seq_list = []
-        self.realigned_seqs = {}
-
         self.parent_dir = os.path.dirname(os.getcwd())
         self.temp_dir = os.path.dirname(os.getcwd()) + "/tmp/"
 
         with open(self.parent_dir + "/settings.toml", 'rb') as f:
             toml = tomllib.load(f)
 
-        self.output_file = output_file
-        self.sam_file = toml["file-paths"]["input-sam"]
-
-        self.fragments_per_seq = toml['settings']['frags-per-seq']
-        self.min_matched_frags = toml['settings']['min-matched-frags']
-
-        self.args_minimap = toml["minimap2"]["options"]
         self.ref_file = toml["minimap2"]["reference-file"]
-
-    def process_sam(self):
-        print("Processing SAM input:")
-
-        with tqdm.tqdm(total=os.path.getsize(self.sam_file)) as pbar:
-            with open(self.sam_file, "r") as infile:
-                for line in infile:
-                    pbar.update(len(line))
-                    if line[0] != '@':
-                        line = line.split('\t')
-                        sequence_name = line[0]
-                        is_aligned = (line[2] != '*')
-                        sequence = line[9]
-
-                        if sequence_name in self.sequence_dict:
-                            self.sequence_dict[sequence_name] = [(is_aligned and self.sequence_dict[sequence_name]),
-                                                                 sequence, sequence_name]
-                            # If sequence is aligned we don't need to store its sequence.
-                            if self.sequence_dict[sequence_name][0]:
-                                self.sequence_dict[sequence_name][1] = None
-
-                        else:
-                            self.sequence_dict[sequence_name] = [is_aligned, sequence, sequence_name]
-
-        for sequence in self.sequence_dict.values():
-            is_aligned = sequence[0]
-            seq_name = sequence[2]
-            sequence = sequence[1]
-            if not is_aligned:
-                self.unaligned_seq_list.append((seq_name, sequence))
-                # if not is_aligned:
-                #     with open(f"{self.temp_dir}/unaligned_seqs.gt", "a") as f:
-                #         f.write(f"{sequence_name}\t{sequence}\n")
-                # self.unaligned_seq_list.append((sequence_name, sequence))
-        del self.sequence_dict
-        print("Completed SAM Processing")
-
-    def fragment_seq(self):
-        print("Slicing unaligned sequences:")
-        with open(f"{self.temp_dir}/unaligned_seq_frags.fasta", "w") as outfile:
-            for sequence_name, sequence in tqdm.tqdm(self.unaligned_seq_list):
-                frag_len = len(sequence) // self.fragments_per_seq
-
-                for i in range(self.fragments_per_seq):
-                    start_idx = i * frag_len
-                    end_idx = start_idx + frag_len if i != self.fragments_per_seq - 1 else len(sequence)
-
-                    outfile.write(f">{sequence_name}_frag_{i + 1}\n")
-                    outfile.write(f"{sequence[start_idx:end_idx]}\n")
-
-        del self.unaligned_seq_list
-        print("Completed Fragment Sequence Processing")
+        self.output_file = output_file
+        self.min_matched_frags = toml['settings']['min-matched-frags']
+        self.args_minimap = toml["minimap2"]["options"]
+        self.realigned_seqs = {}
 
     def realign_fragments(self):
         print("Running minimap2 to realign reads")
@@ -85,7 +26,6 @@ class UnalignedSeqFinder:
             shell=True)
         print("Completed realignment")
 
-    #  Will always be given a .sam file processed to only include aligned sequences (--sam-hit-only)
     def recollect_fragments(self):
         print("Analyzing realigned fragments")
         with tqdm.tqdm(total=os.path.getsize(f"{self.temp_dir}/realigned.sam")) as pbar:
@@ -142,6 +82,72 @@ class UnalignedSeqFinder:
         print(f"Results generated at: HGT/Output/{self.output_file}.txt")
 
 
+class Fragmenter():
+    def __init__(self, output_file="results"):
+        self.sequence_dict = {}
+        self.unaligned_seq_list = []
+        self.parent_dir = os.path.dirname(os.getcwd())
+        self.temp_dir = os.path.dirname(os.getcwd()) + "/tmp/"
+
+        with open(self.parent_dir + "/settings.toml", 'rb') as f:
+            toml = tomllib.load(f)
+
+        self.sam_file = toml["file-paths"]["input-sam"]
+
+        self.fragments_per_seq = toml['settings']['frags-per-seq']
+
+    def find_unaligned_seqs(self):
+        print("Processing SAM input:")
+
+        with tqdm.tqdm(total=os.path.getsize(self.sam_file)) as pbar:
+            with open(self.sam_file, "r") as infile:
+                for line in infile:
+                    pbar.update(len(line))
+                    if line[0] != '@':
+                        line = line.split('\t')
+                        sequence_name = line[0]
+                        is_aligned = (line[2] != '*')
+                        sequence = line[9]
+
+                        if sequence_name in self.sequence_dict:
+                            self.sequence_dict[sequence_name] = [(is_aligned and self.sequence_dict[sequence_name]),
+                                                                 sequence, sequence_name]
+                            # If sequence is aligned we don't need to store its sequence.
+                            if self.sequence_dict[sequence_name][0]:
+                                self.sequence_dict[sequence_name][1] = None
+
+                        else:
+                            self.sequence_dict[sequence_name] = [is_aligned, sequence, sequence_name]
+
+        for sequence in self.sequence_dict.values():
+            is_aligned = sequence[0]
+            seq_name = sequence[2]
+            sequence = sequence[1]
+            if not is_aligned:
+                self.unaligned_seq_list.append((seq_name, sequence))
+
+        del self.sequence_dict
+        print("Completed SAM Processing")
+
+    def fragment_seq(self):
+        print("Slicing unaligned sequences:")
+        with open(f"{self.temp_dir}/unaligned_seq_frags.fasta", "w") as outfile:
+            for sequence_name, sequence in tqdm.tqdm(self.unaligned_seq_list):
+                frag_len = len(sequence) // self.fragments_per_seq
+
+                for i in range(self.fragments_per_seq):
+                    start_idx = i * frag_len
+                    end_idx = start_idx + frag_len if i != self.fragments_per_seq - 1 else len(sequence)
+
+                    outfile.write(f">{sequence_name}_frag_{i + 1}\n")
+                    outfile.write(f"{sequence[start_idx:end_idx]}\n")
+
+        del self.unaligned_seq_list
+        print("Completed Fragment Sequence Processing")
+
+    #  Will always be given a .sam file processed to only include aligned sequences (--sam-hit-only)
+
+
 class SequenceData:
     def __init__(self, sequence):
         self.sequence = sequence
@@ -153,8 +159,12 @@ class SequenceData:
         return f"({self.sequence}, {self.genomes}, {self.fragments}, {self.pairs})"
 
 
-finder = UnalignedSeqFinder()
-finder.process_sam()
+finder = Fragmenter()
+finder.find_unaligned_seqs()
 finder.fragment_seq()
-finder.realign_fragments()
-finder.recollect_fragments()
+del finder
+
+fragAnalyzer = FragAnalyzer()
+fragAnalyzer.realign_fragments()
+fragAnalyzer.recollect_fragments()
+
